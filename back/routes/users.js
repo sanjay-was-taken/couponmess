13d0a5c4826ead7521c14b5ecb70d1f94f7f60e2
ebinthomas06 <ebinthomas1810@gmail.com>
2026-01-01 23:51:53 +0530

    Update users.js

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,21 +3,35 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// CREATE USER
+const bcrypt = require('bcryptjs');
+
 router.post('/', async (req, res) => {
-    const { name, email, role } = req.body;
+    const { name, email, role, password } = req.body;
+
+    if (!password) {
+        return res.status(400).json({ error: "Password is required" });
+    }
+
     try {
-        // Check duplicate email
-        const check = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        const check = await db.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        );
+
         if (check.rows.length > 0) {
             return res.status(400).json({ error: "Email already registered" });
         }
-        // Insert
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
         const result = await db.query(
-            `INSERT INTO users (name, email, role) 
-             VALUES ($1, $2, $3) RETURNING *`,
-            [name, email, role || 'student']
+            `INSERT INTO users (name, email, role, password_hash)
+             VALUES ($1, $2, $3, $4)
+             RETURNING user_id, name, email, role`,
+            [name, email, role || 'student', passwordHash]
         );
+
         res.status(201).json(result.rows[0]);
     } catch (err) {
         console.error(err);
